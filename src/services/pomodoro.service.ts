13a2597a2436abab103fe7c0728706c07ade892a
teHox <orlik.sasha@outlook.com>
2024-06-01 17:36:17 +0200

    init project

---
+++ src/services/pomodoro.service.ts
@@ -0,0 +1,35 @@
+import {
+	IPomodoroSessionResponse,
+	TypePomodoroRoundState,
+	TypePomodoroSessionState,
+} from "@/types/pomodoro.types";
+
+import { axiosWithAuth } from "@/api/interceptors";
+
+class PomodoroService {
+	private BASE_URL = "/user/timer";
+
+	async getTodaySession() {
+		return await axiosWithAuth.get<IPomodoroSessionResponse>(
+			`${this.BASE_URL}/today`,
+		);
+	}
+
+	async createSession() {
+		return await axiosWithAuth.post<IPomodoroSessionResponse>(this.BASE_URL);
+	}
+
+	async updateSession(id: string, data: TypePomodoroSessionState) {
+		return await axiosWithAuth.put(`${this.BASE_URL}/${id}`, data);
+	}
+
+	async deleteSession(id: string) {
+		return await axiosWithAuth.delete(`${this.BASE_URL}/${id}`);
+	}
+
+	async updateRound(id: string, data: TypePomodoroRoundState) {
+		return await axiosWithAuth.put(`${this.BASE_URL}/round/${id}`, data);
+	}
+}
+
+export const pomodoroService = new PomodoroService();
